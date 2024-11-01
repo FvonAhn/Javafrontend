6d86d4eae1b8f22ac3150593834f0a6a1d851315
FvonAhn <fredd24hate@gmail.com>
2024-11-01 13:45:49 +0000

    Added some more faqs

--- src/Components/Question2.jsx
+++ src/Components/Question2.jsx
@@ -4,11 +4,16 @@ import ArrowDown from '../images/arrow-down.svg'
 const Q2 = () => {
 
     return (
-
-        <a href="#" className="faqs">
-            <p className="q-text">What formats can I download my transaction history in?</p>
-            <img src={ArrowDown} className="arrow-down" />  
-        </a>
+        
+        <div className="faqs">
+            <div className="question">
+                <p className="q-text">What formats can I download my transaction history in?</p>
+                <button className="chevron-down"><i className="fa-solid fa-chevron-down"></i></button>  
+            </div>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem velit magni mollitia molestias, commodi deleniti animi. Odio officiis sed temporibus sit expedita at obcaecati?</p>
+            </div>
+        </div>
 
     )
 
