d8ea64d783166a8f6f5042e0ce46825e20bac519
FvonAhn <fredd24hate@gmail.com>
2024-11-11 08:38:37 +0100

    Fixed the issue with the all the FAQs opening at the same time.

--- src/Components/TheFaqs.jsx
+++ src/Components/TheFaqs.jsx
@@ -5,6 +5,7 @@ const TheFaqs = () => {
 
 
     const [faqs, setFaqs] = useState([]);
+    const [openFaqs, setOpenFaqs] = useState({})
 
     useEffect(() => {
 
@@ -27,23 +28,22 @@ const TheFaqs = () => {
         fetchFaqs ()
     }, [])
 
-    const [isOpen, setIsOpen] = useState(false);
-    
-    const toggleQuestion = () => {
-    setIsOpen(!isOpen);
+    const toggleQuestion = (index) => {
+    setOpenFaqs((prevOpenFaqs) => ({...prevOpenFaqs, [index]: !prevOpenFaqs[index]
+    }));
     }; 
 
     return (
 
         <div className="the-faqs">
                 {faqs.map((item, index) => (
-                    <div className="faqs">
-                        <div className="question" onClick={toggleQuestion}>
+                    <div className="faqs" key={index}>
+                        <div className="question" onClick={ () => toggleQuestion(index)}>
                             <p className="q-text">{item.title}</p>
-                            <button className="chevron-down"><i className="fa-solid fa-chevron-down"></i></button>  
+                            <button className={`chevron-down ${openFaqs[index] ? 'open' : ''}`}><i className="fa-solid fa-chevron-down"></i></button>  
                         </div>
-                        <div className={`answer ${isOpen ? 'open' : ''}`}>
-                            {isOpen && <p className="q-text">{item.content}</p> }   
+                        <div className={`answer ${openFaqs[index] ? 'open' : ''}`}>
+                            {openFaqs[index] && <p className="q-text">{item.content}</p> }   
                         </div>
                     </div>
                 ))}
